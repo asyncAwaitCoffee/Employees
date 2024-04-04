7d40c1b7483296a348b8c38eef6581aa3ee0e23b
Andrei Stepanov <steam.oni.am@gmail.com>
2024-04-04 21:32:02 +0400

    css reorganize

---
+++ EmployeesApp/wwwroot/scripts/modal.js
@@ -0,0 +1,18 @@
+﻿const modal = document.querySelector("#modal");
+const messagesList = document.querySelector("#messages-list");
+
+function hideModal() {
+	modal.classList.add("display-none");
+	while (messagesList.firstChild) {
+		messagesList.removeChild(messagesList.firstChild);
+	}
+}
+function showModal(messages) {
+	modal.classList.remove("display-none");
+
+	for (const message of messages) {
+		const li = document.createElement("li");
+		li.textContent = message;
+		messagesList.appendChild(li);
+	}
+}

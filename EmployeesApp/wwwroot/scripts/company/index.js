68511df2dbbdb3458e0690bfa13fcc1a13d51369
Andrei Stepanov <steam.oni.am@gmail.com>
2024-04-04 22:26:56 +0400

    +test data | refactor

---
+++ EmployeesApp/wwwroot/scripts/company/index.js
@@ -0,0 +1,28 @@
+﻿const formCompany = document.querySelector("#form-company");
+const companyImport = document.querySelector("#company-import");
+const companyImportLabel = document.querySelector('label[for="company-import"');
+
+companyImport.addEventListener("change", ev => {
+	companyImportLabel.textContent = ev.target.files[0].name;
+})
+
+formCompany.addEventListener("submit", async (ev) => {
+	ev.preventDefault();
+	const formData = new FormData(formCompany);
+	await fetch("/company/create", {
+		method: "POST",
+		body: formData
+	})
+		.then(response => {
+			companyImport.value = "";
+			companyImportLabel.textContent = "Импортировать из файла"
+			return response.json()
+		})
+		.then(data => {
+			if (!data.ok) {
+				showModal(data.validation);
+			} else {
+				showModal(["Выполнено успешно!"]);
+			}
+		});
+})
---
+++ EmployeesApp/wwwroot/scripts/employee/index.js
@@ -0,0 +1,29 @@
+﻿const formEmployee = document.querySelector("#form-employee");
+const employeeImport = document.querySelector("#employee-import");
+const employeeImportLabel = document.querySelector('label[for="employee-import"');
+
+employeeImport.addEventListener("change", ev => {
+	employeeImportLabel.textContent = ev.target.files[0].name;
+})
+
+formEmployee.addEventListener("submit", (ev) => {
+	ev.preventDefault();
+
+	const formData = new FormData(formEmployee);
+	fetch("/employee/create", {
+		method: "POST",
+		body: formData
+	})
+		.then(response => {
+			employeeImport.value = "";
+			employeeImportLabel.textContent = "Импортировать из файла"
+			return response.json()
+		})
+		.then(data => {
+			if (!data.ok) {
+				showModal(data.validation);
+			} else {
+				showModal(["Выполнено успешно!"]);
+			}
+		});
+})
